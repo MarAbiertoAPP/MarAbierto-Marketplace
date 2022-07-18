456076c1d2297dd7ab123dac578f1b254849a7c7
Nicolas Suarez <2jmk3218@gmail.com>
2022-07-18 05:34:00 -0500

    Rutas place y archivo prechargue

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { chargue } = require("./src/init/preChargueFile.js");//ELIMinar antes de deployar
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(async () => {
+  await chargue();
   server.listen(process.env.PORT, () => {
     console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
