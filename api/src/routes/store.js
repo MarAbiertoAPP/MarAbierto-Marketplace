7dd0813a6e7c7940740609573195a4a182bebd3d
RQuirogaH <rafarqh@gmail.com>
2022-07-18 13:04:19 -0500

    feat(api/src/routes/store): ruta /store/add para agregar un arreglo de nft

--- api/src/routes/store.js
+++ api/src/routes/store.js
@@ -9,4 +9,22 @@ router.get('/all', async (req, res) => {
   res.json(allNFT)
 })
 
+// Ruta post para subir los NFT's
+// recibe un arreglo de objetos, formato (/api/prechargue.json)
+router.post('/add', async (req, res) => {
+  const arrNFT = req.body
+  console.log('aca')
+  const arrPromisesNFT = arrNFT.map(n => {
+    return nft.create({
+      title: n.title,
+      description: n.description,
+      path: n.img,
+      price: n.price
+    })
+  })
+
+  await Promise.all(arrPromisesNFT)
+  res.json({ data: arrNFT })
+})
+
 module.exports = router
