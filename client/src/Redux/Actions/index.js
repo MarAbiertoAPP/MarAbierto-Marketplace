771bae4a64c3909aa9ba8f3d6d975b4c636de42f
jesusmcfly <arenasjesus.w@gmail.com>
2022-07-18 16:51:42 -0400

    update create NFT and get NFT actions

--- client/src/Redux/Actions/index.js
+++ client/src/Redux/Actions/index.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 export function getAllNFT () {
   return function (dispatch) {
-    axios(`http://localhost:${process.env.PORT}`)
+    axios(`http://localhost:${process.env.PORT}/store/all`)
       .then(res => {
         dispatch({ type: GET_ALL_NFT, payload: res.data })
       }).catch(error => console.log(error.message))
@@ -13,7 +13,7 @@ export function getAllNFT () {
 
 export function createNFT (obj) {
   return function (dispatch) {
-    axios.post(`http://localhost:${process.env.PORT}`, obj)
+    axios.post(`http://localhost:${process.env.PORT}/store/add`, obj)
       .then(dispatch({ type: CREATE_NFT }))
       .catch(error => console.log(error.message))
   }
