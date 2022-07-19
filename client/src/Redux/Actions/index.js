f0ae4cb834c440b71eddf644a8a31e87281c6f39
jesusmcfly <arenasjesus.w@gmail.com>
2022-07-19 15:11:40 -0400

    feat(client/redux): new actions a reducer added for the new routes

--- client/src/Redux/Actions/index.js
+++ client/src/Redux/Actions/index.js
@@ -1,9 +1,9 @@
-import { GET_ALL_NFT, CREATE_NFT } from './ActionsCreators'
+import { GET_ALL_NFT, CREATE_NFT, FILTER_BY_PRICE, FILTER_BY_TITLE, FILTER_BY_CATEGORY, FILTER_BY_STATE, FILTER_BY_USER, CREATE_USER } from './ActionsCreators'
 import axios from 'axios'
 
 export function getAllNFT () {
   return function (dispatch) {
-    axios(`http://localhost:${process.env.PORT}/store/all`)
+    axios(`http://localhost:${process.env.PORT}/stores/all`)
       .then(res => {
         dispatch({ type: GET_ALL_NFT, payload: res.data })
       }).catch(error => console.log(error.message))
@@ -12,8 +12,58 @@ export function getAllNFT () {
 
 export function createNFT (obj) {
   return function (dispatch) {
-    axios.post(`http://localhost:${process.env.PORT}/store/add`, obj)
+    axios.post(`http://localhost:${process.env.PORT}/stores`, obj)
       .then(dispatch({ type: CREATE_NFT }))
       .catch(error => console.log(error.message))
+  }
+}
+
+export function filterByPrice (min, max) {
+  return function (dispatch) {
+    axios(`http://localhost:${process.env.PORT}/stores/nft?price=${min}_${max}`)
+      .then(res =>
+        dispatch({ type: FILTER_BY_PRICE, payload: res.data }))
+      .catch(error => console.log(error.message))
+  }
+}
+
+export function filterByCategory (categoryId) {
+  return function (dispatch) {
+    axios(`http://localhost:${process.env.PORT}/stores/nft?categoryID=${categoryId}`)
+      .then(res => {
+        dispatch({ type: FILTER_BY_CATEGORY, payload: res.data })
+      }).catch(error => console.log(error.message))
+  }
+}
+export function filterByTitle (title) {
+  return function (dispatch) {
+    axios(`http://localhost:${process.env.PORT}/stores/nft?title=${title}`)
+      .then(res => {
+        dispatch({ type: FILTER_BY_TITLE, payload: res.data })
+      })
+  }
+}
+export function filterByUser (userId) {
+  return function (dispatch) {
+    axios(`http://localhost:${process.env.PORT}/stores/nft?userId=${userId}`)
+      .then(res => {
+        dispatch({ type: FILTER_BY_USER, payload: res.data })
+      }).catch(error => console.log(error))
+  }
+}
+// filter if the user is active or not
+export function FilterByState (state) {
+  return function (dispatch) {
+    axios(`http://localhost:${process.env.PORT}/stores/nft?isActive=${state}`)
+      .then(res => {
+        dispatch({ type: FILTER_BY_STATE, payload: res.data })
+      }).catch(error => console.log(error))
+  }
+}
+export function createUser (obj) {
+  return function (dispatch) {
+    axios.post(`http://localhost:${process.env.PORT}/users/signup`, obj)
+      .then(dispatch({ type: CREATE_USER }))
+      .catch(error => console.log(error))
   }
 }
