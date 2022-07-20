d4513a9932de623cdfd997d971bbd688bee5d8a3
joaquinmzumelzu <joaquinmunoztrade@gmail.com>
2022-07-20 01:03:28 -0400

    cambio de fondo a morado jeje

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react'
 import './App.css'
 import Home from './Components/Home/Home.jsx'
 import Landing from './Components/LandingPage/Landing.jsx'
+import Faq from './Components/Faq/faq.jsx'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default function App () {
@@ -9,8 +11,10 @@ export default function App () {
     <Fragment>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
           <Route path="/" element={<Landing/>}></Route>
+          <Route path="/home" element={<Home />} />
+          <Route path="/faq" element={<Faq/>}></Route>
+
         </Routes>
       </BrowserRouter>
     </Fragment>
