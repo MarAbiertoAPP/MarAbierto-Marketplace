d53890348b0167cfbc7b5b403aa41610ab1c667e
julianluissoto <julianlasoto@hotmail.com>
2022-07-20 12:45:59 -0300

    created Footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,16 @@ import './App.css'
 import Home from './Components/Home/Home.jsx'
 import Landing from './Components/LandingPage/Landing.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Faq from '../src/Components/Faq/faq'
 
 export default function App () {
   return (
     <Fragment>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Landing/>}></Route>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/" element={<Landing/>}/>
+          <Route path="/faq" element={<Faq/>}/>
         </Routes>
       </BrowserRouter>
     </Fragment>
