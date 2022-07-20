e331228d14a26b49637143655945268d9a3b34ad
julianluissoto <julianlasoto@hotmail.com>
2022-07-20 12:56:24 -0300

    FOOTER CREATED

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import footer from '../Footer/Footer.module.css'
 import { Link } from 'react-router-dom'
+
 const Footer = () => {
   return (
     <div className={footer.Container}>
