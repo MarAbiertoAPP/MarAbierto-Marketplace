fbbbc900140b72f700b799d2deda970ce656eddc
marcelogodoybaquero <marcelo.godoy@mi.unc.edu.ar>
2022-07-22 12:28:52 -0300

    hay alerts nuevas para el carrito, no son SWET ALERT!, no se duplican items!

--- client/src/Components/UI/Card/Card.jsx
+++ client/src/Components/UI/Card/Card.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Classes from './card.module.css'
 import PropTypes from 'prop-types'
 import { useAuth0 } from '@auth0/auth0-react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addToCart } from '../../../Redux/Actions/ActionsCart'
 import { Link } from 'react-router-dom'
 
 export default function Card ({ title, image, price, id }) {
+  const { Cart } = useSelector(state => state)
   const dispatch = useDispatch()
   Card.propTypes = {
     title: PropTypes.string,
@@ -16,35 +17,39 @@ export default function Card ({ title, image, price, id }) {
   }
   const { isAuthenticated, loginWithRedirect } = useAuth0()
   const handleBuy = () => {
-    return dispatch(
-
-      addToCart({
-        title,
-        image,
-        price,
-        id
-      }
+    if (Cart.find(i => i.id === id)) {
+      return alert('the current item its on the shopping cart, go check it')
+    } else {
+      alert('item added to the shopping cart!')
+      return dispatch(
+        addToCart({
+          title,
+          image,
+          price,
+          id
+        }
+        )
       )
-    )
+    }
   }
 
   return (
     <div className={Classes.container}>
-        <div className={Classes.card}>
-            <div className={Classes.imgBx}>
-              <Link to={`/detail/${id}`}>
-                <img src={image} alt="pic"></img>
-              </Link>
-            </div>
-            <div className={Classes.contentBx}>
-                <h2>{title}</h2>
-                <div className={Classes.color}>
-                    <h3>Price :</h3>
-                    <span>{price} ETH</span>
-                </div>
-               <button onClick={(e) => isAuthenticated ? handleBuy(e) : loginWithRedirect()} >Buy Now</button>
-            </div>
+      <div className={Classes.card}>
+        <div className={Classes.imgBx}>
+          <Link to={`/detail/${id}`}>
+            <img src={image} alt="pic"></img>
+          </Link>
         </div>
+        <div className={Classes.contentBx}>
+          <h2>{title}</h2>
+          <div className={Classes.color}>
+            <h3>Price :</h3>
+            <span>{price} ETH</span>
+          </div>
+          <button onClick={(e) => isAuthenticated ? handleBuy(e) : loginWithRedirect()}>Buy Now</button>
         </div>
+      </div>
+    </div>
   )
 }
