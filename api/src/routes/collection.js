5437a7291e0b04a2161e0d0cfe857216c9e16675
Nicolas Suarez <2jmk3218@gmail.com>
2022-07-28 10:09:41 -0500

    Add collections back

---
+++ api/src/routes/collection.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const collectionController = require('../controllers/collection.js')
+
+/**
+ * @author Nicolas Alejandro Suarez
+ * @param {} sequelize
+ */
+/**
+ * route to post a place
+ */
+router.post('/', collectionController.createCollection)
+/**
+ * route to get a place
+ */
+// router.get('/', categoryController.getCategory)
+
+module.exports = router
