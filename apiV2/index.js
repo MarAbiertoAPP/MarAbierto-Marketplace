39c70e1b936d3953b6268aa0d188b58dc343fc95
jesusmcfly <arenasjesus.w@gmail.com>
2022-08-08 18:02:38 -0400

    resteo de db

--- apiV2/index.js
+++ apiV2/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js')
 // const chargue = require('./precharge/index')
 
 // Syncing all the models at once.nxnbxzbnbzbznxbnzbksjksj
-conn.sync({ force: false }).then(async () => {
+conn.sync({ force: true }).then(async () => {
   // await chargue()
   server.listen(process.env.PORT, () => {
     console.log(`%s listening at ${process.env.PORT}`) // eslint-disable-line no-console
