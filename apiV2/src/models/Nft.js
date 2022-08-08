e6626934334fca11ca651347dc5b3acc121e6f31
jesusmcfly <arenasjesus.w@gmail.com>
2022-08-08 18:42:56 -0400

    new try

--- apiV2/src/models/Nft.js
+++ apiV2/src/models/Nft.js
@@ -42,7 +42,8 @@ module.exports = (sequelize) => {
     },
     isBanned: {
       type: DataTypes.BOOLEAN,
-      defaultValue: false
+      defaultValue: false,
+      allowNull: false
     }
   }, {
     timeStamps: false,
