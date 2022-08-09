9ba82da5d5c5d6b3b5cfd383742f37a9f64ec542
NotReply <NotReply@DESKTOP-SJBVQ5F>
2022-08-09 10:42:04 -0300

    agregue boton para retroceder de pagina

--- client/src/Components/UI/Nav/Navigation.jsx
+++ client/src/Components/UI/Nav/Navigation.jsx
@@ -10,7 +10,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { IoIosCart } from 'react-icons/io'
 import Cart from '../../Cart/Cart'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { resetFilters } from '../../../Redux/Actions'
 import LanguajeButton from '../../LanguajeButton/LanguajeButton'
 
@@ -23,6 +23,8 @@ function Nav () {
   const navigate = useNavigate()
 
   const dispatch = useDispatch()
+  const location = useLocation()
+  console.log(location)
 
   const showDropdown = () => {
     setOpen(!open)
@@ -38,20 +40,31 @@ function Nav () {
   const handleClick = (e) => {
     e.preventDefault()
     dispatch(resetFilters())
-    navigate('/')
+    navigate(-1)
   }
   return (
 
     <div className='w-full max-w-7xl'>
       <nav className={Classes.nav}>
-        <div onClick={(e) => handleClick(e)}>
+          {location.pathname !== '/' &&
+        <div className={Classes.back}>
+          <button onClick={(e) => handleClick(e)} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <span className="relative px-4 py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Back
+            </span>
+          </button>
+          </div>
+          }
+          <Link to ='/'>
+        <div>
           <img src={Logo} alt="logo" className={`${Classes.logo} cursor-pointer`} />
         </div>
+          </Link>
 
         <ul className={Classes.navbar_menu}>
-        <LanguajeButton/>
+          <LanguajeButton />
           {cart.length > 0 && (<li style={{ color: '#8a00ff', fontWeight: 'bolder', fontSize: '1.5em' }}>
-            {cart.length }
+            {cart.length}
           </li>)}
 
           <li onClick={() => showCart()}>
@@ -67,8 +80,8 @@ function Nav () {
           <li>
             {isAuthenticated
               ? <Link to='/user'>
-            <img src={User} alt="user" className={Classes.iconsOn} />
-            </Link>
+                <img src={User} alt="user" className={Classes.iconsOn} />
+              </Link>
               : <img src={User} alt="user" className={Classes.icons} />
             }
           </li>
@@ -96,8 +109,8 @@ function Nav () {
         </ul>
       </nav>
 
-         <div className= 'w-full mt-20' >
-      <iframe className={'w-full h-10'} src={'https://s.tradingview.com/embed-widget/ticker-tape/?locale=en&page-uri=https%3A%2F%2Fwww.tradingview.com%2Fwidget%2Fticker-tape%2F#%7B%22symbols%22%3A%5B%7B%22description%22%3A%22ETH%2FUSD%22%2C%22proName%22%3A%22BITSTAMP%3AETHUSD%22%7D%2C%7B%22description%22%3A%22USD%2FCLP%22%2C%22proName%22%3A%22FX_IDC%3AUSDCLP%22%7D%2C%7B%22description%22%3A%22USD%2FARS%22%2C%22proName%22%3A%22FX_IDC%3AUSDARS%22%7D%2C%7B%22description%22%3A%22USD%2FCOP%22%2C%22proName%22%3A%22FX_IDC%3AUSDCOP%22%7D%5D%2C%22showSymbolLogo%22%3Atrue%2C%22colorTheme%22%3A%22dark%22%2C%22isTransparent%22%3Afalse%2C%22displayMode%22%3A%22adaptive%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A78%2C%22utm_source%22%3A%22www.tradingview.com%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22ticker-tape%22%7D'}></iframe>
+      <div className='w-full mt-20' >
+        <iframe className={'w-full h-10'} src={'https://s.tradingview.com/embed-widget/ticker-tape/?locale=en&page-uri=https%3A%2F%2Fwww.tradingview.com%2Fwidget%2Fticker-tape%2F#%7B%22symbols%22%3A%5B%7B%22description%22%3A%22ETH%2FUSD%22%2C%22proName%22%3A%22BITSTAMP%3AETHUSD%22%7D%2C%7B%22description%22%3A%22USD%2FCLP%22%2C%22proName%22%3A%22FX_IDC%3AUSDCLP%22%7D%2C%7B%22description%22%3A%22USD%2FARS%22%2C%22proName%22%3A%22FX_IDC%3AUSDARS%22%7D%2C%7B%22description%22%3A%22USD%2FCOP%22%2C%22proName%22%3A%22FX_IDC%3AUSDCOP%22%7D%5D%2C%22showSymbolLogo%22%3Atrue%2C%22colorTheme%22%3A%22dark%22%2C%22isTransparent%22%3Afalse%2C%22displayMode%22%3A%22adaptive%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A78%2C%22utm_source%22%3A%22www.tradingview.com%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22ticker-tape%22%7D'}></iframe>
       </div>
 
     </div>
