dc48ef689a00d450bee9dcace5fb9ffde3e11314
NotReply <NotReply@DESKTOP-SJBVQ5F>
2022-08-10 15:12:39 -0300

    marce metele media pila

--- client/src/Components/Banned/Banned.jsx
+++ client/src/Components/Banned/Banned.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import ChromeDinoGame from 'react-chrome-dino'
 
 export default function Banned () {
   return (
     <div>
-      <h1>You are banned!</h1>
-      <p>You have been banned from this site. If you believe this is an error, please contact the site administrator.</p>
+      <ChromeDinoGame />
     </div>
   )
 }
