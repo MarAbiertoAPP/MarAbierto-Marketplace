4d125eef4c297108930d0f6e3bbcad325da2fc01
NotReply <NotReply@DESKTOP-SJBVQ5F>
2022-08-10 10:17:22 -0300

    mantiene el favorito al recargar o volver a entrar falta el delete

--- client/src/Components/DetailsNFT/TitleLikesXL/TitleLikesXL.jsx
+++ client/src/Components/DetailsNFT/TitleLikesXL/TitleLikesXL.jsx
@@ -1,30 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { FaHeart } from 'react-icons/fa'
 import { useSelector } from 'react-redux'
 import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 const TitleLikesXL = (props) => {
   TitleLikesXL.propTypes = {
     title: PropTypes.any
   }
   const userId = useSelector(state => state.User?.id)
-  const { detail } = useSelector(state => state)
+  const { id } = useParams()
+
   const { Favorites } = useSelector(state => state)
   const [estilo, setEstilo] = useState('text-neutral-400 text-3xl hover:text-red-500')
-  const favorito = Favorites.find(favorite => favorite.nftId === detail.id)
+  const favorito = Favorites.map((e) => e.id).find((e) => e === Number(id))
 
   const handleClick = () => {
-    axios.post('/favorite/add', { nftId: detail.nftId, userId })
+    axios.post('/favorite/add', { nftId: id, userId })
     return setEstilo('text-red-500 text-3xl')
     // axios.post('/favorite/delete', { nftId: id, userId })
   }
+
+  useEffect(() => {
+    if (favorito) {
+      setEstilo('text-red-500 text-3xl')
+    }
+  }, [Favorites])
 
   return (
     <div className='hidden xl:flex w-full'>
       <h1 className='mt-6 text-3xl tracking-wider text-purple-700 mx-8 capitalize'>{props.title}</h1>
       <div className='flex items-center mt-6 place-content-end ml-64'>
-        {favorito && <FaHeart className={estilo} onClick={handleClick}/>}
+       <FaHeart className={estilo} onClick={handleClick}/>
         {/* <p className='ml-4 text-md self-auto text-neutral-400 tracking-wider'>444 likes</p> */}
       </div>
     </div>
