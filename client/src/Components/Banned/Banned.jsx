ff10f4a71aafe08870135b91c05e77522ab8edd2
NotReply <NotReply@DESKTOP-SJBVQ5F>
2022-08-10 23:31:01 -0300

    agregue ntfs a slabs

--- client/src/Components/Banned/Banned.jsx
+++ client/src/Components/Banned/Banned.jsx
@@ -1,10 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ChromeDinoGame from 'react-chrome-dino'
 
 export default function Banned () {
-  useEffect(() => {
-
-  }, [])
   return (
     <div className='bg-white'>
       <ChromeDinoGame />
